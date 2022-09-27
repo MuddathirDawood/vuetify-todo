cdf26f614e5bbf240e72ccfc392c84498d5a10be
MuddathirDawood <dmuddathir@gmail.com>
2022-09-27 14:56:26 +0200

    vuetify

---
+++ src/views/ToDo.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-list
+    flat
+  >
+      <v-list-item>
+        <template v-slot:default="{ active, }">
+          <v-list-item-action>
+            <v-checkbox
+              :input-value="active"
+              color="primary"
+            ></v-checkbox>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Notifications</v-list-item-title>
+          </v-list-item-content>
+        </template>
+      </v-list-item>
+      
+  </v-list>
+</template>
+
+<script>
+export default ({
+name: 'HomeView',
+});
+</script>
